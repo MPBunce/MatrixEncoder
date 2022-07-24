1f5e2e5657f2c380e1fd61daea5a4301199df577
Matthew Bunce <mbunce@uoguelph.ca>
2022-07-23 21:48:30 -0400

    the

--- main.cpp
+++ main.cpp
@@ -1,75 +1,84 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <ctime>
+#include <thread>
 using namespace std;
 
 //Declaring Functions
-
+vector<vector<int>> makeMatrix(int n);
+vector<vector<int>> MatrixMultiply(vector<vector<int>> A, vector<vector<int>> B, int n);
 
 int main() {
 
-    std::cout << "\nHello! Welcome to the matix multiplier we are going to genereate two NxN matrices and multiply them.\n";
     int n = 0;
+    std::cout << "\nHello! Welcome to the matix multiplier we are going to genereate two NxN matrices and multiply them.\n";
     std::cout << "Enter the size of the NxN matices you want multuply: ";
     std::cin >> n;
-
     std::cout << "\n\n\nMatrix A is as follows:\n";
 
-    //Matrix A
-    vector<vector<int>> matrixA(n, vector<int>(n));
+    int timeOne = clock();
+    //Matrix A    std::cout << "\n\n\nThe results of A*B are as follows:\n";
+    std::cout << "\nMatrix A: ";
+    vector<vector<int>> matrixA = makeMatrix(n);
 
+    //Matrix B
+    std::cout << "\nMatrix B: ";  
+    vector<vector<int>> matrixB = makeMatrix(n);
+
+    //Multipilcation Time
+    std::cout << "\nResult: ";  
+    vector<vector<int>> Result = MatrixMultiply(matrixA, matrixB, n); 
+
+    int timeTwo = clock();
+    int totalTime = timeTwo - timeOne;
+    std::cout << "\n\n\n\nThe total time taken in clock units was: " << totalTime << "\n";
+
+    //Multithreading
+    int next = 0;
+    std::cout << "\n\nWe will now try multithreading this, please press enter yes(y) to continue: \n";
+    std::cin >> next;
+
+
+    return 0;
+}
+
+
+vector<vector<int>> makeMatrix(int n){
+
+    vector<vector<int>> matrix(n, vector<int>(n));
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
             
             //Go through every position and set value
-            matrixA[i][j] = (1 + (rand() % 10000));
+            matrix[i][j] = (1 + (rand() % 10000));
 
         }
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++){
-            std::cout << matrixA[i][j] <<" ";
+            std::cout << matrix[i][j] <<" ";
         }   
         std::cout<<endl;         
     }
 
-    std::cout << "\n\n\nMatrix B is as follows:\n";
+    return matrix;
+}
 
-    //Matrix B
-    vector<vector<int>> matrixB(n, vector<int>(n));
-    for(int i = 0; i < n; ++i){
-        for(int j = 0; j < n; ++j){
-
-            //Go through every position and set value
-            matrixB[i][j] = 1 + (rand() % 10000);
-        
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++){
-            std::cout << matrixB[i][j] <<" ";
-        } 
-        std::cout<<endl;     
-    }
-
-    //Multipilcation Time\
-
+vector<vector<int>> MatrixMultiply(vector<vector<int>> A, vector<vector<int>> B, int n) {
     
-
     vector<vector<int>> matrixResult(n, vector<int>(n));
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             int sum =0;
             for(int k=0; k<n; k++){
-                sum += (matrixA[i][k] * matrixB[k][j]);
+                sum += (A[i][k] * B[k][j]);
             }
             matrixResult[i][j] = sum;
         }
     }
-
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++){
@@ -78,7 +87,5 @@ int main() {
         std::cout<<endl;         
     }
 
-
-    return 0;
+    return matrixResult;
 }
-
